9d7a045f8f0c1d0481c9f65e9faf61743e37c29f
Kanak <kkupgrader.fs@gmail.com>
2023-06-06 22:56:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from './pages/home/Home';
 import Student from './pages/student/Student';
 import FindMyRd from './pages/FindMyRd/FindMyRd';
 import Footer from './components/Footer';
-import Login from './pages/backend/Login.jsx';
+
 function App() {
   return (
     <div className="App">
@@ -17,7 +17,6 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/r/:rollNo" element={<Student />} />
           <Route exact path="/FindMyRd" element={<FindMyRd />} />
-          <Route path="/admin/" element={<Login />} />
         </Routes>
       </Router>
       <Footer />
