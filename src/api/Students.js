073bab3679d20a8a505db2a0f95867e024a6792a
Kanak <kanakkholwal@gmail.com>
2025-07-13 17:32:41 +0530

    refactor: update dependencies and migrate to Vite
    - Updated dependencies in package.json to latest versions, including React, Firebase, and Workbox.
    - Changed build and start scripts to use Vite instead of React Scripts.
    - Removed public/index.html and migrated to a new index.html structure.
    - Reorganized import statements in src/api/Students.js for clarity.
    - Updated GitHub and CodePen links in Footer component.
    - Deleted unused files: src/index.js and src/reportWebVitals.js.
    - Added new src/index.jsx file for Vite entry point.
    - Created vite.config.js for Vite configuration and plugins.

--- src/api/Students.js
+++ src/api/Students.js
@@ -1,7 +1,7 @@
-import batch_21 from "./students_21.json";
-import batch_20 from "./students_20.json";
-import batch_19 from "./students_19.json";
 import { ranking, uniq } from "../scripts/customFunctions";
+import batch_19 from "./students_19.json";
+import batch_20 from "./students_20.json";
+import batch_21 from "./students_21.json";
 
 const _batches = [batch_21, batch_20]
 let data = [];
@@ -18,20 +18,21 @@ const correspondingRanks = ranking(data, (a, b) => parseFloat(b.semesters[b.seme
 data.forEach((stud, index) => {
     stud.College_Rank = correspondingRanks[index];
 });
+
 // Year Rank  (Increasing Order)
-Batches.forEach((batch) => {
+for (const batch of Batches) {
     const correspondingBatchRanks = ranking(data.filter(({ Batch }) => Batch === batch), (a, b) => parseFloat(b.semesters[b.semesters.length - 1].cgpi) > parseFloat(a.semesters[a.semesters.length - 1].cgpi));
     data.filter(({ Batch }) => Batch === batch).forEach((stud, index) => {
         stud.Batch_Rank = correspondingBatchRanks[index];
     });
-});
+}
 // Branch Rank  (Increasing Order)
-Branches.forEach((branch) => {
+for (const branch of Branches) {
     const correspondingBranchRanks = ranking(data.filter(({ Branch }) => Branch === branch), (a, b) => parseFloat(b.semesters[b.semesters.length - 1].cgpi) > parseFloat(a.semesters[a.semesters.length - 1].cgpi));
     data.filter(({ Branch }) => Branch === branch).forEach((stud, index) => {
         stud.Branch_Rank = correspondingBranchRanks[index];
     });
-});
+}
 
 export const Students = (Batch, Branch) => {
     let fault = batch_19
